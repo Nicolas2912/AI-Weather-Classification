e0d44075e3c55697538aec516bd88548fc6fbc0a
Nicolas Schneider <nicolas.schneider@fh-bielefeld.de>
2024-05-13 14:59:25 +0200

    Add files to new docuement structure

---
+++ src/prediction/predict.py
@@ -0,0 +1,106 @@
+import os
+import sys
+from typing import Tuple, Dict
+
+import structlog
+import argparse
+import torch
+from tabulate import tabulate
+
+logger = structlog.get_logger()
+
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Add the project root to the sys.path
+sys.path.insert(0, PROJECT_ROOT)
+
+from train.train_model import WeatherClassifier
+from utils.data_loader import WeatherDataset
+
+
+def arguemnts() -> Tuple[str, str, str, str]:
+    """
+    Parses command-line arguments for the weather classification prediction script.
+
+    Returns:
+        tuple: A tuple containing:
+               - model_path (str): Path to the trained model file.
+               - images_path (str): Path to the directory containing images to predict.
+               - dataset_path (str): Path to the dataset directory.
+               - device (str): The computation device to use ('cpu' or 'cuda').
+    """
+    parser = argparse.ArgumentParser(description='Predict weather class for own images.')
+
+    parser.add_argument('--model', type=str,
+                        default=r"C:\Users\Anwender\Desktop\Nicolas\Dokumente\FH Bielefeld\Optimierung und Simulation\2. Semester\SimulationOptischerSysteme\AI-Weather-Classification\train\trained_model_new.pth",
+                        help='Path to the trained model.')
+    parser.add_argument('--images', type=str,
+                        default=r"C:\Users\Anwender\Desktop\Nicolas\Dokumente\FH Bielefeld\Optimierung und Simulation\2. Semester\SimulationOptischerSysteme\AI-Weather-Classification\test_image2",
+                        help='Path to the images to predict.')
+    parser.add_argument('--dataset', type=str,
+                        default=r"C:\Users\Anwender\Desktop\Nicolas\Dokumente\FH Bielefeld\Optimierung und Simulation\2. Semester\SimulationOptischerSysteme\AI-Weather-Classification\dataset",
+                        help='Path to the dataset.')
+    parser.add_argument('--device', type=str, default='cpu', help='Device to use for prediction.',
+                        choices=["cpu", "cuda", "mps"])
+
+    args = parser.parse_args()
+
+    return args.model, args.images, args.dataset, args.device
+
+
+def predict(model_path: str, images_path: str, dataset_path: str, compute_device: str) -> Dict[str, str]:
+    """
+    Loads a trained model and performs predictions on a set of images.
+
+    Args:
+        model_path (str): Path to the trained PyTorch model file.
+        images_path (str): Path to the directory containing images for prediction.
+        dataset_path (str): Path to the dataset directory used to initialize the dataset structure.
+        compute_device (str): The device to perform computation on e.g. 'cpu' or 'cuda'.
+
+    Returns:
+        dict: A dictionary where keys are image filenames and values are the predicted weather conditions.
+    """
+    predictions = dict()
+    compute_device = torch.device(compute_device)
+
+    # Get the number of classes from the (training)-dataset
+    n_classes = len(os.listdir(dataset_path))
+
+    # Load the model
+    model = WeatherClassifier(num_classes=n_classes, compute_device=compute_device, verbosity=False)
+    model.load_state_dict(torch.load(model_path))
+
+    # Get the prediction dataset
+    weather_dataset = WeatherDataset(data_folder=dataset_path)
+    data_loader = weather_dataset.custom_image_loader(images_path)
+    logger.info(f"Number of images found in in images folder: {len(data_loader.dataset)}\n")
+
+    # Perform predictions
+    for i, (image, image_path) in enumerate(data_loader):
+        image_filename = data_loader.dataset.samples[i][0]
+        image_filename = image_filename.split("\\")[-1]
+        image = image.to(device)
+        output = model(image)
+        _, predicted = torch.max(output, 1)
+        predictions[image_filename] = weather_dataset.dataset.classes[predicted.item()]
+
+    table = tabulate(predictions.items(), headers=['Image', 'Predicted Weather'], tablefmt='orgtbl')
+    logger.info("Predictions:\n")
+    print(table)
+
+    print("\n=======FINISHED=======")
+
+    return predictions
+
+
+if __name__ == "__main__":
+    model_path, images_path, dataset_path, device = arguemnts()
+
+    logger.info("Parameters:")
+    print(f"Model path: {model_path}")
+    print(f"Images path: {images_path}")
+    print(f"Dataset path: {dataset_path}")
+    print(f"Device: {device}\n")
+
+    predict(model_path, images_path, dataset_path, device)
---
+++ src/utils/data_loader.py
@@ -0,0 +1,191 @@
+import os
+import sys
+import random
+from collections import defaultdict
+from typing import Tuple, Dict
+
+import torch
+from torch.utils.data import DataLoader, Dataset, random_split
+import torchvision.transforms as transforms
+from torchvision.datasets import ImageFolder
+import matplotlib.pyplot as plt
+import numpy as np
+from tqdm import tqdm
+
+from torchvision.transforms import Resize, ToTensor
+
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Add the project root to the sys.path
+sys.path.insert(0, PROJECT_ROOT)
+
+class WeatherDataset(Dataset):
+    """A dataset class for loading weather images with transformations.
+
+    Attributes:
+        data_folder (str): Path to the dataset folder.
+        transform (callable, optional): Optional transform to be applied on a sample.
+
+    Methods:
+        __len__: Returns the total number of images in the dataset.
+        __getitem__: Retrieves an image and its label by index.
+        get_data_loaders: Splits the dataset into training, validation, and test sets and returns corresponding loaders.
+    """
+
+    def __init__(self, data_folder: str, transform=None, resize_format: tuple = (512, 512)):
+        self.data_folder = data_folder
+        self.resize_format = resize_format
+        if transform is None:
+            transform = transforms.Compose([Resize(self.resize_format), ToTensor()])
+        self.transform = transform
+        self.dataset = ImageFolder(self.data_folder, transform=self.transform)
+        # Calculate all classes
+        self.classes = self.dataset.classes
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, index):
+        img, label = self.dataset[index]
+        return img, label
+
+    def get_data_loaders(self, batch_size=32, train_ratio=0.7, val_ratio=0.15, seed=42) -> \
+            Tuple[DataLoader, DataLoader, DataLoader]:
+        """Creates and returns data loaders for the dataset after splitting into train, validation, and test sets.
+
+        Args:
+            batch_size (int): Number of samples in each batch.
+            train_ratio (float): Fraction of the dataset to be used as training data.
+            val_ratio (float): Fraction of the dataset to be used as validation data.
+            seed (int): Seed for random operations to ensure reproducibility.
+
+        Returns:
+            tuple: Three DataLoader instances for training, validation, and test datasets.
+        """
+        if seed is not None:
+            torch.manual_seed(seed)
+            random.seed(seed)
+            np.random.seed(seed)
+
+        total_length = len(self)
+        train_length = int(train_ratio * total_length)
+        val_length = int(val_ratio * total_length)
+        test_length = total_length - train_length - val_length
+
+        train_dataset, val_dataset, test_dataset = random_split(
+            self, [train_length, val_length, test_length],
+            generator=torch.Generator().manual_seed(seed)
+        )
+
+        train_data_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+        val_data_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
+        test_data_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+
+        return train_data_loader, val_data_loader, test_data_loader
+
+    def custom_image_loader(self, image_path: str):
+        """Loads a single image from the dataset and returns a DataLoader instance.
+
+        Args:
+            image_path (str): Path to the image file.
+
+        Returns:
+            DataLoader: DataLoader instance containing the image.
+        """
+        imgs = ImageFolder(image_path, transform=self.transform)
+        imgs_loader = DataLoader(imgs, batch_size=1, shuffle=False)
+        return imgs_loader
+
+
+def analyze_class_distribution(data_loaders: dict, index_to_class: dict) -> Dict[str, Dict[str, int]]:
+    """
+    Analyzes the class distribution across training, validation, and test datasets.
+
+    Args:
+    - data_loaders (dict): Dictionary of DataLoaders.
+    - index_to_class (dict): Dictionary mapping label indices to class names.
+
+    Returns:
+    - distribution (dict): Dictionary containing class distributions for train, val, and test sets.
+    """
+    class_distribution = {'train': defaultdict(int), 'val': defaultdict(int), 'test': defaultdict(int)}
+
+    for phase in ['train', 'val', 'test']:
+        loader = data_loaders[phase]
+        for images, labels in tqdm(loader, desc=f"Analyzing {phase} distribution"):
+            for label in labels:
+                class_name = index_to_class[label.item()]
+                class_distribution[phase][class_name] += 1
+
+    return class_distribution
+
+
+def plot_class_distributions(class_distribution: Dict[str, Dict[str, int]]):
+    """
+    Plots pie charts of class distributions for training, validation, and test sets.
+
+    Args:
+    - distribution (dict): Dictionary containing class distributions for train, val, and test sets.
+    """
+    # Define the number of subplots based on the distribution keys
+    n = len(class_distribution)
+    fig, axs = plt.subplots(1, n, figsize=(n * 5, 6))  # Adjust fig-size as needed
+
+    if n == 1:  # If only one phase is provided, wrap axs in a list
+        axs = [axs]
+
+    for ax, (phase, counts) in zip(axs, class_distribution.items()):
+        # Prepare data for the pie chart
+        labels = counts.keys()
+        sizes = counts.values()
+        total = sum(sizes)  # Total count for percentage calculation
+
+        ax.pie(sizes, labels=labels, autopct=lambda p: '{:.1f}%'.format(p) if p > 0 else '', startangle=90)
+        ax.set_title(f'{phase.capitalize()} Set Distribution (Total: {total})')
+        ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+    plt.tight_layout()
+    plt.show()
+
+
+if __name__ == "__main__":
+    path_dataset = ("/Users/nicolasschneider/MeineDokumente/FH_Bielefeld/Optimierung_und_Simulation/2. Semester"
+                    "/SimulationOptischerSysteme/AI-Weather-Classification/dataset")
+    one_image = ("/Users/nicolasschneider/MeineDokumente/FH_Bielefeld/Optimierung_und_Simulation/2. Semester"
+                 "/SimulationOptischerSysteme/AI-Weather-Classification/utils/one_image")
+
+    path_dataset_win = (r"C:\Users\Anwender\Desktop\Nicolas\Dokumente\FH Bielefeld\Optimierung und Simulation"
+                        r"\2. Semester\SimulationOptischerSysteme\AI-Weather-Classification\dataset")
+    one_image_win = (r"C:\Users\Anwender\Desktop\Nicolas\Dokumente\FH Bielefeld\Optimierung und Simulation"
+                     r"\2. Semester\SimulationOptischerSysteme\AI-Weather-Classification\test_image")
+
+    W = WeatherDataset(data_folder=path_dataset_win)
+    train_loader, val_loader, test_loader = W.get_data_loaders(batch_size=1, train_ratio=0.7, seed=42)
+
+    print(f"Number of images in the dataset: {len(W)}")
+    print(f"Number of images in the training set: {len(train_loader.dataset)}")
+    print(f"Number of images in the validation set: {len(val_loader.dataset)}")
+    print(f"Number of images in the test set: {len(test_loader.dataset)}\n")
+
+    class_to_idx = W.dataset.class_to_idx
+    idx_to_class = {v: k for k, v in class_to_idx.items()}
+
+    loaders = {
+        'train': train_loader,
+        'val': val_loader,
+        'test': test_loader
+    }
+
+    # distribution = analyze_class_distribution(loaders, idx_to_class)
+    # plot_class_distributions(distribution)
+
+    # Load a single image from the dataset
+    image_loader = W.custom_image_loader(one_image_win)
+    image, _ = next(iter(image_loader))
+    # Display the image and its label
+    plt.imshow(image.squeeze().permute(1, 2, 0))
+    plt.title(f"Custom image")
+    plt.axis('off')
+    plt.show()
